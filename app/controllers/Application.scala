9e1059a69f686d3c2ad849f56be1c26c41712b0f
Richard Hull <rm_hull@yahoo.co.uk>
2013-01-15 23:58:50 +0000

    Headers grafted onto response

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,6 +8,17 @@ import play.api.libs.iteratee._
 import play.api.libs.ws.{WS, ResponseHeaders}
 import play.api.Play.current
 import java.util.concurrent.TimeUnit._
+import scala.collection.JavaConverters._
+
+import com.ning.http.client.{
+  AsyncHttpClient,
+  RequestBuilderBase,
+  FluentCaseInsensitiveStringsMap,
+  HttpResponseBodyPart,
+  HttpResponseHeaders,
+  HttpResponseStatus,
+  Response => AHCResponse
+}
 
 object Application extends Controller {
 
@@ -24,6 +35,8 @@ object Application extends Controller {
           timeout => RequestTimeout)
       }
     }
+
+    //Ok.stream(rawData(request.uri))
   }
 
   def streamFrom[A](uri: String)(consumer: ResponseHeaders => Iteratee[Array[Byte], A]) = {
@@ -39,8 +52,16 @@ object Application extends Controller {
   def convert(response: play.api.libs.ws.Response) = {
     val data = response.ahcResponse.getResponseBodyAsStream
     val dataContent: Enumerator[Array[Byte]] = Enumerator.fromStream(data)
-    // TODO use withHeaders
-    Status(response.status).stream(dataContent).withHeaders("X-Proxied-by" -> "kebab").as(response.header(CONTENT_TYPE).getOrElse(""))
+    Status(response.status)
+      .stream(dataContent)
+      .withHeaders(ningHeadersToMap(response.ahcResponse.getHeaders).toArray:_*)
+      .withHeaders("X-Proxied-by" -> "kebab")
+      .as(response.header(CONTENT_TYPE).getOrElse("plain/text"))
   }
+
+  private def ningHeadersToMap(headers: FluentCaseInsensitiveStringsMap) =
+    mapAsScalaMapConverter(headers).asScala.map { case (k,v) =>
+      k -> v.asScala.mkString(",")
+    }.toMap
 }
 
